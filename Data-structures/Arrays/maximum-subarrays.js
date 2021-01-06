fc70f9fe44553c8bd8eaca567f79e0e45a2f3c96
eugeneganshin <e.s.ganshin.study@gmail.com>
2021-01-06 21:33:32 +0300

    kadanets alhorithm

--- Data-structures/Arrays/maximum-subarrays.js
+++ Data-structures/Arrays/maximum-subarrays.js
@@ -1,24 +1,19 @@
+// Kadanet's Algorithm
 function maxSub(array) {
-
-    let max = 0
+    let len = array.length
+    let local_max = 0
+    let global_max = 0
 
     for (let i = 0; i < array.length; i++) {
-        const sum = max + array[i]
-        
-        if (array[i] < 0 && sum < 0) {
-            max = 0
-        } else {
-            // max += array[i]
-            max = Math.max(max, max+=array[i])
-            console.log({i, val: array[i], max})
-            
+        local_max = Math.max(array[i], array[i] + local_max)
+        if (local_max > global_max) {
+            global_max = local_max
         }
     }
-    
-    console.log(max, 'max')
-    return max
 
+    return global_max
 }
 
 // console.log(maxSub([4,-1]))
-maxSub([-2,1,-3,4,-1,2,1,-5,4])
+console.log(maxSub([-2,1,-3,4,-1,2,1,-5,4]))
+//      0, 1, 0, 4, 3
