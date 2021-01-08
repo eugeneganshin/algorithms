303da8e951708d32af783e62dafc303e9f05fb31
eugeneganshin <e.s.ganshin.study@gmail.com>
2021-01-08 13:12:05 +0300

    hash tables

---
+++ Data-structures/Hash-tables/index.js
@@ -0,0 +1,7 @@
+let user = {
+    age: 54,
+    name: 'Kylie',
+    magic: true,
+    scream: () => console.log('ahhh')
+}
+
---
+++ Data-structures/Hash-tables/readme.md
@@ -0,0 +1,6 @@
+# Hash tables time complexity
+
+* insert O(1)
+* lookup O(1) -- rarily O(n)
+* delete O(1)
+* search O(1)
